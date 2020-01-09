e4e4ddd89a8fba119bfd8dc4895e71a3353dc44d
Shital Patel <shital732@yahoo.com>
2020-01-09 12:37:02 -0500

    add css

---
+++ assets/javascripts/game.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var word = [
+    "sholay",
+    "lagaan",
+    "pyaasa",
+    "awara",
+    "anand",
+    "devdas",
+    "chakde",
+    "barfi",
+    "don"
+  ];
+  var wins = 0;
+  var loss = 0;
+  var guessesLeft = 9;
+  var guessSoFar = [];
+  var guessesText = document.getElementById("wrong-guess");
+  var guessesLeftText = document.getElementById("guess-left");
+  var winsText = document.getElementById("win");
+  var lossesText = document.getElementById("loss");
+  var userChoiceText = document.getElementById("new-word");
+});
